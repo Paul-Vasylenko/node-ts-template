1e543427d27daf00284ece67e58e12908e7dd261
Vasylenko Paul <maincrafter1307@gmail.com>
2023-01-19 20:57:58 +0200

    Feat: basic setup

---
+++ build/index.js
@@ -0,0 +1,3 @@
+import { testFn } from './test/a.js';
+console.log(testFn(1, 4));
+//# sourceMappingURL=index.js.map
---
+++ build/test/a.js
@@ -0,0 +1,4 @@
+export function testFn(p1, p2) {
+  return p1 + p2;
+}
+//# sourceMappingURL=a.js.map
